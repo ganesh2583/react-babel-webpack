20f224a08d1cba07c71097a2adb362491ede1096
Ganesh Chaitanya Kale <ganesh2583@gmail.com>
2020-01-25 20:16:33 -0500

    Added Jest configuration.

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,6 @@ const Dotenv = require("dotenv-webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-
 module.exports = {
   // Use the src/index.js file as entry point to bundle it.
   // If the src/index.js file imports other JavaScript files, bundle them as well.
@@ -20,7 +19,7 @@ module.exports = {
     new Dotenv(),
     new HtmlWebpackPlugin({
       template: "./index.html",
-    })
+    }),
   ],
   // Informing Webpack on which files to use the loader (e.g. .js and jsx files)
   // and optionally which folders to exclude from the process (e.g. node_modules):
@@ -30,24 +29,24 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         // Use babel loader to load js and jsx files.
-        use: ["babel-loader", "eslint-loader"]
+        use: ["babel-loader", "eslint-loader"],
       },
       {
         test: /\.(css|scss)$/,
         // Use style, css and sass loader for style files.
-        use: ["style-loader", "css-loader", "sass-loader"]
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         // Use file loader for image files.
         loader: "file-loader",
         options: {
-          name: "[name].[ext]"
-        }
-      }
-    ]
+          name: "[name].[ext]",
+        },
+      },
+    ],
   },
   resolve: {
-    extensions: ["*", ".js", ".jsx"]
-  }
+    extensions: ["*", ".js", ".jsx"],
+  },
 };
